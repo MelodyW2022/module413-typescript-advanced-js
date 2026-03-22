69cc156187c93fa98ab72f7bf2f411dbdf290584
Sheung Wan Wong <melodywonghxy@hotmail.com>
2026-03-21 18:31:16 -0700

    feat: implement PhysicalProduct with weight getter and tax override

--- lab1-inventory-tracker/src/models/PhysicalProduct.ts
+++ lab1-inventory-tracker/src/models/PhysicalProduct.ts
@@ -1,3 +1,18 @@
 import { Product } from "./Product";
 
-export class PhysicalProduct extends Product {}
+export class PhysicalProduct extends Product {
+  private _weight: number;
+
+  constructor(sku: string, name: string, price: number, weight: number) {
+    super(sku, name, price);
+    this._weight = weight;
+  }
+  getPriceWithTax(taxRate: number = 0.1): number {
+    //Override the getPriceWithTax() method to calculate a final price that includes a 10% tax rate.
+    return this.price * (1 + taxRate);
+  }
+
+  public get weight(): string {
+    return `${this._weight.toFixed(1)}kg`;
+  }
+}
